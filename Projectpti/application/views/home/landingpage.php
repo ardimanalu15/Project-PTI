fb54262be9a6737ec8ec42ed115a8f3658e1238d
Your Name <ardimanalu02@gmail.com>
2021-04-05 16:15:50 +0700

    landingpage

--- Projectpti/application/views/home/landingpage.php
+++ Projectpti/application/views/home/landingpage.php
@@ -5,11 +5,104 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="css/landingpage.css">
 </head>
 
 <body>
-    <img src="img/landingpage1.png" alt="background">
+    <div class="halaman-utama">
+
+        <div class="navbar">
+            <div class="brand"> </div>
+            <ul class="nav-link">
+                <li> <a href="#" class="navbar-link"> Home </a></li>
+                <li> <a href="#" class="navbar-link"> Profil </a></li>
+                <li> <a href="#" class="navbar-link"> Guru </a></li>
+                <li> <a href="#" class="navbar-link">Info Pendaftaran </a></li>
+                <li> <a href="#" class="butt" type="button">Login</a>
+            </ul>
+        </div>
+        <!--Header -->
+        <section class="main">
+            <div class="container">
+                <div class="main-content">
+                    <div class="text">
+                        <h1> Selamat Datang Di Website, <br /></h1>
+                        <img src="img/TK.png" alt="gambar tk">
+                    </div>
+                </div>
+                <div class="banner">
+                    <img src="img/landingpage2.png" alt="gambar2">
+                </div>
+                <div class="sosmet">
+                    <div class="wa">
+                        <a href="#"><img src="img/wa.png" alt="whatshap"></a>
+                    </div>
+                    <div class="fb">
+                        <a href="#"><img src="img/fb.png" alt="facebook"></a>
+                    </div>
+                    <div class="ig">
+                        <a href="#"><img src="img/ig.png" alt="instagram"></a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- akhir Header -->
+
+
+
+        <!-- akhir isi -->
+        <section class="content">
+            <div class="contain">
+                <div class="batang"></div>
+                <div class="batang2"></div>
+                <div class="batang3"></div>
+                <div class="retangle">
+                </div>
+                <div class="title">
+                    <dir class="text">
+                        <h1 class="profile">Profile</h1>
+                        <h3 class="intro">Taman kanak-kanak ini sudah berdiri sejak
+                            tahun 1996.
+                            TK Melati Putih dapat menerima
+                            sekitar 50 hingga 60 peserta didik setiap tahunnya,
+                            ada 4 orang staf yang bekerja sebagai tenaga
+                            pendidik di TK ini.
+                        </h3>
+                    </dir>
+                </div>
+                <div class="lanjutprofile">
+                    <a href="#"><img src="img/Group131.png" alt="tombol"></a>
+                </div>
+            </div>
+        </section>
+        <!-- akhir isi -->
+
+        <!-- profile guru -->
+        <section class="guru">
+            <div class="contenguru">
+                <div class="batang4"></div>
+                <div class="batang5"></div>
+                <div class="batang6"></div>
+                <div class="isiguru">
+                    <div class="isigurukiri">
+                        <h1> GURU</h1>
+                        <div class="contenguruu">
+                            <div class="text-1">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="isigurukanan">
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- akhir guru -->
+
+    </div>
 </body>
 
 </html>
