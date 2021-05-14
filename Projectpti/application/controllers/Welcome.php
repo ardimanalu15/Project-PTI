37452c6d6e99582c7ee6524adab82f6ff77f4040
Your Name <ardimanalu02@gmail.com>
2021-05-14 20:14:44 +0700

    Merge branch 'main' of https://github.com/ardimanalu15/Project-PTI

--- Projectpti/application/controllers/Welcome.php
+++ Projectpti/application/controllers/Welcome.php
@@ -1024,4 +1024,21 @@ class Welcome extends CI_Controller
 			redirect('Welcome/StatusSppCon');
 		}
 	}
+	public function Cetakpdf()
+	{
+		$this->load->library('dompdf_gen');
+
+		$data['keuangan'] =  $this->db->get('keuangan')->result_array();
+
+		$this->load->view('home/Cetak', $data);
+
+		$paper_size = 'A4';
+		$orientation = 'landscape';
+		$html = $this->output->get_output();
+		$this->dompdf->set_paper($paper_size, $orientation);
+
+		$this->dompdf->load_html($html);
+		$this->dompdf->render();
+		$this->dompdf->stream("Laporan_Keuangan.pdf", array('Attachment' => 0));
+	}
 }
